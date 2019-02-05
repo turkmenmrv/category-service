fd31a8792a6eaaa5d3f406fd45ba41087bfa469b
Merve Sert <turkmen.mrv@gmail.com>
2019-02-05 03:47:13 +0100

    postgre error fix

--- target/classes/db/local_bootstrap.sh
+++ target/classes/db/local_bootstrap.sh
@@ -3,7 +3,7 @@ CONTAINER_NAME="postgres-docker"
 docker rm -f ${CONTAINER_NAME} || true
 docker run --name ${CONTAINER_NAME} -p 5432:5432 -d postgres
 
-#sleep 2
+sleep 3
 docker cp ./create-db.sql postgres-docker:/docker-entrypoint-initdb.d/create-db.sql
 
 docker exec -u postgres postgres-docker psql postgres postgres -f docker-entrypoint-initdb.d/create-db.sql
